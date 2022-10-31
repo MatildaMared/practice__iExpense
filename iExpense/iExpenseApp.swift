31dc9332fcf10fc243377ddbeedcfe31d36f460c
Matilda Mared <matildamared@live.se>
2022-10-31 08:58:26 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Matilda Mared on 2022-10-31.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
